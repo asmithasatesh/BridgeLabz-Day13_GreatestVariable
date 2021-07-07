9f6fdc7626833806cf5118f77408be6901b70afb
asmithasatesh <asmithasatesh@gmail.com>
2021-07-07 20:19:40 +0530

    [Asmitha]Find greatest using integer type

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,13 +1,42 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Generics_UnitTesting;
+using System;
 namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        GreatestNum obj;
+        [TestInitialize]
+        public void SetUp()
         {
+            obj = new GreatestNum();
+
+        }
+        [TestMethod]
+        public void UseCase1_1()
+        {
+            int first = 100, second = 30, third = 10, actualValue, expectedValue;
+            expectedValue = first;
+            actualValue = obj.LargestNumber(first, second, third);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void UseCase1_2()
+        {
+            int first = 103, second = 300, third = 10, actualValue, expectedValue;
+            expectedValue = second;
+            actualValue = obj.LargestNumber(first, second, third);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+        [TestMethod]
+        public void UseCase1_3()
+        {
+            int first = 10, second = 30, third = 100, actualValue, expectedValue;
+            expectedValue = third;
+            actualValue = obj.LargestNumber(first, second, third);
+            Assert.AreEqual(expectedValue, actualValue);
         }
     }
 }
