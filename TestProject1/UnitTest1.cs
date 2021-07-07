5abb150652bc152b642427e7b82932fe16d56b8c
asmithasatesh <asmithasatesh@gmail.com>
2021-07-07 20:35:48 +0530

    [Asmitha]Find greatest using float type

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -13,6 +13,7 @@ namespace TestProject1
             obj = new GreatestNum();
 
         }
+        //USE CASE 1
         [TestMethod]
         public void UseCase1_1()
         {
@@ -38,5 +39,31 @@ namespace TestProject1
             actualValue = obj.LargestNumber(first, second, third);
             Assert.AreEqual(expectedValue, actualValue);
         }
+        [TestMethod]
+        //USE CASE 2
+        public void UseCase2_1()
+        {
+            float first = 100.1F, second = 30.4F, third = 10.6F, expectedValue, actualValue;
+            expectedValue = first;
+            actualValue = obj.LargestNumberFloat(first, second, third);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+        [TestMethod]
+        public void UseCase2_2()
+        {
+            float first = 10.1F, second = 300.4F, third = 10.6F, expectedValue, actualValue;
+            expectedValue = second;
+            actualValue = obj.LargestNumberFloat(first, second, third);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+        [TestMethod]
+        public void UseCase2_3()
+        {
+            float first = 10, second = 30, third = 100, actualValue, expectedValue;
+            expectedValue = third;
+            actualValue = obj.LargestNumberFloat(first, second, third);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+        //USE CASE 3
     }
 }
